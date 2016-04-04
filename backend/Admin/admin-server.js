dd08cee1d6127b7d99f65e17a9ec9ff35cf0e4ce
chat-dev-group <chat.dev.group@gmail.com>
2016-04-04 13:37:45 +0300

    Admin server

--- backend/Admin/admin-server.js
+++ backend/Admin/admin-server.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
 	var gkConst 	  =  require('./gk-questions-constants')();
-	var gkQService   =  require('./gk-questions-service')(app, authConst);
+	var gkQService   =  require('./gk-questions-service')(app;
   
     app.post(gkConst.getAllUrl, function(req, res) {
-    	gkQService.getAll();
+    	gkQService.getAll(res);
     });
 
 };
